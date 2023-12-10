5a6198c27e3e060c0415f451fc316cc91843ab59
Matej Muller <matejmuller@Matejs-MacBook-Air.local>
2023-12-10 20:58:08 +0100

    State i dinamički css

--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,11 +1,15 @@
-export default function TabButton({ children, onSelect }) {
+export default function TabButton({ children, onSelect, isSelected }) {
     // function handleClick() {
     //     console.log('HELLO');
     // }
-
     return (
         <li>
-            <button onClick={onSelect}>{children}</button>
+            <button
+                className={isSelected ? 'active' : undefined}
+                onClick={onSelect}
+            >
+                {children}
+            </button>
         </li>
     );
 }
