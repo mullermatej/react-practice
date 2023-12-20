0ee450d99be67cb13c398bbfc50af9de333e9b64
Matej Muller <matejmuller@Matejs-MacBook-Air.local>
2023-12-20 19:01:50 +0100

    Two way binding, Lifting state up

--- 02-tic-tac-toe-project/src/App.jsx
+++ 02-tic-tac-toe-project/src/App.jsx
@@ -1,20 +1,36 @@
+import { useState } from 'react';
 import Player from './components/Player';
+import GameBoard from './components/GameBoard';
 
 function App() {
+    const [activePlayer, setActivePlayer] = useState('X');
+
+    function handleSelectSquare() {
+        setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
+    }
+
     return (
         <main>
             <div id="game-container">
-                <ol id="players">
+                <ol
+                    id="players"
+                    className="highlight-player"
+                >
                     <Player
-                        name={'Player 1'}
+                        initialName={'Player 1'}
                         symbol={'X'}
+                        isActive={activePlayer === 'X'}
                     />
                     <Player
-                        name={'Player 2'}
+                        initialName={'Player 2'}
                         symbol={'O'}
+                        isActive={activePlayer === 'O'}
                     />
                 </ol>
-                GAME BOARD
+                <GameBoard
+                    onSelectSquare={handleSelectSquare}
+                    activePlayerSymbol={activePlayer}
+                />
             </div>
             LOG
         </main>
--- 02-tic-tac-toe-project/src/components/Player.jsx
+++ 02-tic-tac-toe-project/src/components/Player.jsx
@@ -1,28 +1,35 @@
 import { useState } from 'react';
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
+    const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
         setIsEditing((editing) => !editing);
     }
 
-    let playerName = <span className="player-name">{name}</span>;
+    function handleChange(event) {
+        console.log(event);
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
 
     if (isEditing) {
-        playerName = (
+        editablePlayerName = (
             <input
                 type="text"
                 required
-                value={name}
+                value={playerName}
+                onChange={handleChange}
             />
         );
     }
 
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
