3c4942210d8d5321872b3b9941e2e238ed228e0a
Matej Muller <matejmuller@Matejs-MacBook-Air.local>
2023-12-21 12:47:47 +0100

    Computed values, removing unnecessary state management, sharing state across components

--- 02-tic-tac-toe-project/src/App.jsx
+++ 02-tic-tac-toe-project/src/App.jsx
@@ -1,12 +1,62 @@
 import { useState } from 'react';
+
 import Player from './components/Player';
 import GameBoard from './components/GameBoard';
+import Log from './components/Log';
+import { WINNING_COMBINATIONS } from './winning-combinations.js';
+
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+];
+
+function deriveActivePlayer(gameTurns) {
+    let currentPlayer = 'X';
+
+    if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+        currentPlayer = 'O';
+    }
+
+    return currentPlayer;
+}
 
 function App() {
-    const [activePlayer, setActivePlayer] = useState('X');
+    const [gameTurns, setGameTurns] = useState([]);
+    // const [activePlayer, setActivePlayer] = useState('X');
 
-    function handleSelectSquare() {
-        setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
+    const activePlayer = deriveActivePlayer(gameTurns);
+
+    let gameBoard = initialGameBoard;
+
+    for (const turn of gameTurns) {
+        const { square, player } = turn;
+        const { row, col } = square;
+
+        gameBoard[row][col] = player;
+    }
+
+    let winner;
+
+    for (const combination of WINNING_COMBINATIONS) {
+        const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+        const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+        const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+        if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
+            winner = firstSquareSymbol;
+        }
+    }
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        // setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
+        setGameTurns((prevTurns) => {
+            const currentPlayer = deriveActivePlayer(prevTurns);
+
+            const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevTurns];
+
+            return updatedTurns;
+        });
     }
 
     return (
@@ -27,12 +77,13 @@ function App() {
                         isActive={activePlayer === 'O'}
                     />
                 </ol>
+                {winner && <p>You won, {winner}!</p>}
                 <GameBoard
                     onSelectSquare={handleSelectSquare}
-                    activePlayerSymbol={activePlayer}
+                    board={gameBoard}
                 />
             </div>
-            LOG
+            <Log turns={gameTurns} />
         </main>
     );
 }
