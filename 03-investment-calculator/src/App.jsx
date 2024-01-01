1924063f46fdd67cdfc8ae9b1bca301f35d9445c
Matej Muller <matejmuller@Matejs-MacBook-Air.local>
2024-01-01 16:38:06 +0100

    Dovršavanje Results komponente

--- 03-investment-calculator/src/App.jsx
+++ 03-investment-calculator/src/App.jsx
@@ -1,15 +1,39 @@
+import { useState } from 'react';
+
 import Header from './components/Header';
 import UserInput from './components/UserInput';
-import Result from './components/Result';
+import Results from './components/Results';
 
 function App() {
-    return (
-        <>
-            <Header />
-            <UserInput />
-            <Result />
-        </>
-    );
+	const [userInput, setUserInput] = useState({
+		initialInvestment: 10000,
+		annualInvestment: 1200,
+		expectedReturn: 6,
+		duration: 10,
+	});
+
+	const inputIsValid = userInput.duration >= 1;
+
+	function handleChange(inputIdentifier, newValue) {
+		setUserInput((prevUserInput) => {
+			return {
+				...prevUserInput,
+				[inputIdentifier]: +newValue,
+			};
+		});
+	}
+
+	return (
+		<>
+			<Header />
+			<UserInput
+				userInput={userInput}
+				onChange={handleChange}
+			/>
+			{!inputIsValid && <p className="center"> Please enter a duration greater than zero.</p>}
+			{inputIsValid && <Results input={userInput} />}
+		</>
+	);
 }
 
 export default App;
--- 03-investment-calculator/src/components/Header.jsx
+++ 03-investment-calculator/src/components/Header.jsx
@@ -1,13 +1,13 @@
 import logo from '../assets/investment-calculator-logo.png';
 
 export default function Header() {
-    return (
-        <div id="header">
-            <img
-                src={logo}
-                alt="Investment logo"
-            />
-            <h1>Investment Calculator</h1>
-        </div>
-    );
+	return (
+		<div id="header">
+			<img
+				src={logo}
+				alt="Investment logo"
+			/>
+			<h1>Investment Calculator</h1>
+		</div>
+	);
 }
